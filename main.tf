cfefd09862a4a7f51450122c6de88544422a84e7
owner <ali-salehi@ali-Linux.digi.box>
2024-12-10 13:23:14 +0100

    V03-key

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "Ali" {
-  ami = "ami-05edb7c94b324f73c"
-
+  ami           = "ami-05edb7c94b324f73c"
+  key_name      = "salehi"
   instance_type = "t3.micro"
 
   tags = {
+    Name       = "AliAgha"
     Created_By = "terraform"
   }
 }
