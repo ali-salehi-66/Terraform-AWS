212218c9bc8bfb5bbe5efd3d31951c03bd0d7eaa
owner <ali-salehi@ali-Linux.digi.box>
2024-12-11 19:21:50 +0100

    V06 - S3

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
+
+
+
 terraform {
+
+  backend "s3" {
+    bucket = "alisalehifereydooni"
+    key    = "build/fereydonkenar/terraform.tfstate"
+    region = "eu-north-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
