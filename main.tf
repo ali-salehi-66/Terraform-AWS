bd5fe3072b9bd51cd81dfa5ad7cdadc05faf3d32
owner <ali-salehi@ali-Linux.digi.box>
2024-12-22 12:48:44 +0100

    import

--- main.tf
+++ main.tf
@@ -8,13 +8,28 @@ resource "aws_instance" "Ali" {
     Created_By = "terraform"
   }
 }
-#resource "aws_instance" "Ali1" {
- # ami           = var.ami_id
-  #key_name      = var.key_name
-  #instance_type = var.instance_type
+#terraform import aws_instance.Ali1 i-09a01d3c563ce7626
+resource "aws_instance" "Ali1" {
+  ami           = "ami-05edb7c94b324f73c"
+  key_name      = var.key_name
+  instance_type = "t3.micro"
 
-  #tags = {
-   # Name       = "sepid"
-   # Created_By = "terraform"
-  #}
+  tags = {
+    Name       = "aliagha2"
+    Created_By = "terraform"
+  }
+}
+
+
+
+
+#resource "aws_instance" "Ali1" {
+# ami           = var.ami_id
+#key_name      = var.key_name
+#instance_type = var.instance_type
+
+#tags = {
+# Name       = "sepid"
+# Created_By = "terraform"
+#}
 #}
